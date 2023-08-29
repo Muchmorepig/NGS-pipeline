d388ea14f5209e99ff0ecf67b5c8829f19654656
万木春 <68462246+Muchmorepig@users.noreply.github.com>
2023-08-29 11:18:52 +0800

    Update get_summits_broadPeak.py

--- cuttag/get_summits_broadPeak.py
+++ cuttag/get_summits_broadPeak.py
@@ -1,12 +1,3 @@
-#!/usr/bin/python
-# Copyright (C) 2019 Qian Zhu
-#
-# CUT&RUNTools is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; version 2 of the License.
-#
-# CUT&RUNTools is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-#
-# A copy of the GNU General Public License has been distributed along with CUT&RUNTools and is found in LICENSE.md.
-
 import sys
 import os
 import re
