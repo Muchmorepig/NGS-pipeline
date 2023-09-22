d9c66616d5e2b320fd237ce206002f3350529092
Muchmorepig <muchmorepig@gmail.com>
2023-09-22 13:40:21 +0800

    change cuttag.sh

--- CutTag/cuttag.sh
+++ CutTag/cuttag.sh
@@ -159,47 +159,16 @@ sleep 0.5
 
 echo >&2 -e "[info] Trim: ${B}$trim${N}"
 
+
 if [ "$trim" == "true" ]; then
-  if ! command -v fastp &>/dev/null; then
-    echo -e "${R}Need tool: fastp ...${N}"
-    exit
-  fi
-
-  if [ ! -d "${odir}/clean" ]; then
-    echo -e "  ${G}Created directory: ${odir}/clean to store trimed data${N}"
-    mkdir -p ${odir}/clean
-  fi
-  sleep 0.1
-
-  fplog=${odir}/log/fastp
-  if [ ! -d "$fplog" ]; then
-    echo -e "  ${G}Created directory: ${fplog} to store 'fastp' log${N} \n"
-    mkdir -p ${fplog}
-  fi
-  sleep 0.3
-  #
-  date >&2
-  echo >&2 -e "[info] Trimming file in $indir use 'fastp'\n"
-
-  ff=$(ls ${indir}/*.gz | sed 's/_R[12]\..*//' | uniq)
-
-  for i in $ff; do
-    base=$(basename ${i})
-    echo -e "  ${G}Sample: ${base}${N}"
-    fastp --thread $th \
-      -i ${i}_R1.fq.gz -I ${i}_R2.fq.gz \
-      -o ${odir}/clean/${base}_R1.fq.gz -O ${odir}/clean/${base}_R2.fq.gz \
-      -j ${fplog}/${base}.fastp.json -h /dev/null \
-      2>${fplog}/${base}.logs
-  done
-
-  date >&2
-  echo >&2 -e "[info] Trimming has finished \n"
+  source ${script_path}/src/fastp_cmd.sh
+  trim_files $indir $odir $th
 else
   date >&2
-  echo >&2 -e "[info] Skip Trim ... \n"
+  echo >&2 -e "[info] Skipping Trim ... \n"
   sleep 0.2
 fi
+
 
 echo >&2 -e "[info] Align: ${B}$align${N}"
 
