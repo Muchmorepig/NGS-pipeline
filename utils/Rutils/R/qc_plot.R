27c77036dd45e5c8610a9a6f25e823933a08c0d8
Muchmorepig <muchmorepig@gmail.com>
2024-10-08 15:34:08 +0800

    change qc

--- utils/Rutils/R/qc_plot.R
+++ utils/Rutils/R/qc_plot.R
@@ -1,8 +1,8 @@
+#' @import ggplot2
+#' @import dplyr
+#' @import patchwork
 #' @export
 
-require(ggplot2)
-require(patchwork)
-require(dplyr)
 qc_plot <- function(obj = scrna,
                     max_nFeature = 6000,
                     min_nFeature = 500,
@@ -11,8 +11,15 @@ qc_plot <- function(obj = scrna,
                     color = c("percent.mito", "percent.chlo")) {
   # Helper function to create violin plot with thresholds
   create_violin_plot <- function(obj, feature, min_val, max_val, add_thresholds = TRUE) {
-    p <- suppressMessages(VlnPlot(obj, features = feature, layer = "counts"))
-
+    p <- suppressMessages(VlnPlot(obj, features = feature, layer = "counts")) +
+      scale_y_continuous(expand = c(0, 0)) +
+      theme(
+        axis.text.x = element_blank(),
+        axis.title.x = element_blank(),
+        axis.line.x = element_blank(),
+        axis.ticks.x = element_blank(),
+        legend.position = "none"
+      )
     if (add_thresholds) {
       p <- p +
         geom_hline(yintercept = min_val, linetype = "dashed") +
@@ -25,38 +32,27 @@ qc_plot <- function(obj = scrna,
   p_feature <- create_violin_plot(obj, "nFeature_RNA", min_nFeature, max_nFeature)
   p_count <- create_violin_plot(obj, "nCount_RNA", min_nCount, max_nCount)
 
-  p3 <- FeatureScatter(obj, feature1 = "nCount_RNA", feature2 = "nFeature_RNA") +
-    theme(legend.position = "none")
+  p_scatter <- FeatureScatter(obj, feature1 = "nCount_RNA", feature2 = "nFeature_RNA") +
+    theme(legend.position = "none") +
+    geom_hline(yintercept = c(min_nFeature, max_nFeature), linetype = "dashed") +
+    geom_vline(xintercept = c(min_nCount, max_nCount), linetype = "dashed")
 
-  p2 <- (p_feature | p_count) &
-    scale_y_continuous(expand = c(0, 0)) &
-    theme(
-      axis.text.x = element_blank(),
-      axis.title.x = element_blank(),
-      axis.line.x = element_blank(),
-      axis.ticks.x = element_blank(),
-      legend.position = "none"
-    )
-  p1 <- p2 | (p3 + geom_hline(yintercept = min_nFeature, linetype = "dashed") +
-    geom_hline(yintercept = max_nFeature, linetype = "dashed") + geom_vline(xintercept = min_nCount, linetype = "dashed") +
-    geom_vline(xintercept = max_nCount, linetype = "dashed"))
+  p_main <- p_feature | p_count | p_scatter
 
-  if (!is.null(color) && color != "") {
+  if (!is.null(color) && all(color != "") && length(color) > 0) {
     plist_color <- lapply(color, function(f) create_violin_plot(obj, f, min_nFeature, max_nFeature, add_thresholds = FALSE))
 
     dd <- as_tibble(obj[[]], rownames = "Cell.Barcode")
 
     plist2 <- lapply(color, function(col) {
       dd <- dd %>% arrange(!!sym(col))
-      ggplot(dd, aes_string(x = "nCount_RNA", y = "nFeature_RNA", color = col)) +
+      ggplot(dd, aes(x = nCount_RNA, y = nFeature_RNA, color = !!sym(col))) +
         geom_point(size = 1, alpha = 0.9) +
         theme_classic() +
         scale_color_gradientn(
           colors = c("grey", "blue", "green2", "red", "yellow"),
           guide = guide_legend(title = col, title.position = "top")
         ) +
-        geom_hline(yintercept = min_nFeature, linetype = "dashed") +
-        geom_hline(yintercept = max_nFeature, linetype = "dashed") +
         scale_y_continuous(expand = c(0, min(min_nFeature - 200, 0.05 * max_nFeature))) +
         theme(legend.position = c(0.8, 0.3))
     })
@@ -65,9 +61,8 @@ qc_plot <- function(obj = scrna,
     p_color <- p_color & NoLegend() & theme(axis.title = element_blank(), axis.text.x = element_blank())
     p_gradient <- wrap_plots(plist2) + plot_layout(ncol = length(color))
 
-    return(p1 / (p_color | p_gradient))
+    return(p_main / (p_color | p_gradient))
   }
 
-  return(p1)
+  return(p_main)
 }
-
