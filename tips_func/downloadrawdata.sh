1155c514e0a314dd0b2f7e4c6eda0db47c090cee
Muchun Wan <68462246+Muchmorepig@users.noreply.github.com>
2024-12-06 11:26:35 +0800

    Delete tips_func directory

--- tips_func/downloadrawdata.sh
+++
@@ -1,9 +0,0 @@
-cat wget.txt|while read id;
-do
-	arr=($id)
-	name=${arr[0]}
-	url=${arr[1]}
-	echo $name
-	echo ${url}
-	wget -c ${url} -O ./${name}
-done		
--- tips_func/wget_down.sh
+++
@@ -1,7 +0,0 @@
-cat md5_link.txt|while read id; do
-	base=${id##*Data/}
-	name=${base%%\?*}
-	n=$(echo ${name} | sed 's/\//_/g')
-	echo ${n}
-	wget -O ${n} ${id} &
-done
